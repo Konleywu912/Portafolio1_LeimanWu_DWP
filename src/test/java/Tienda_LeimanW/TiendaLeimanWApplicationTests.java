f98c3d6539502e19cd2bf12dde5b4952b69e9859
Konleywu912 <leymanwu912@gmail.com>
2026-01-27 16:48:17 -0600

    Initial commit

---
+++ src/test/java/Tienda_LeimanW/TiendaLeimanWApplicationTests.java
@@ -0,0 +1,13 @@
+package Tienda_LeimanW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaLeimanWApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
