77acc6d4eb9f36405ff2f6b1edc357aede31420f
Konleywu912 <leymanwu912@gmail.com>
2026-02-11 12:12:42 -0600

    semana#4

--- src/main/java/Tienda_LeimanW/service/CategoriaService.java
+++ src/main/java/Tienda_LeimanW/service/CategoriaService.java
@@ -6,17 +6,26 @@ package Tienda_LeimanW.service;
 
 import Tienda_LeimanW.domain.Categoria;
 import Tienda_LeimanW.repository.CategoriaRepository;
+import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class CategoriaService {
 
     @Autowired
     private CategoriaRepository categoriaRepository;
-    
+
+    // === Código de la imagen ===
+    @Autowired
+    private FirebaseStorageService firebaseStorageService;
+    // ===========================
+
     @Transactional(readOnly=true)
     public List<Categoria> getCategorias(boolean activo) {
         if (activo) {
@@ -24,6 +33,41 @@ public class CategoriaService {
         }
         return categoriaRepository.findAll();
     }
-    
-    
+
+    // === Código de la imagen ===
+    @Transactional(readOnly = true)
+    public Optional<Categoria> getCategoria(Integer idCategoria) {
+        return categoriaRepository.findById(idCategoria);
+    }
+
+    @Transactional
+    public void save(Categoria categoria, MultipartFile imagenFile) {
+        categoria = categoriaRepository.save(categoria);
+        if (!imagenFile.isEmpty()) { //Si no está vacio... pasaron una imagen...
+            try {
+                String rutaImagen = firebaseStorageService.uploadImage(
+                        imagenFile, "categoria",
+                        categoria.getIdCategoria());
+                categoria.setRutaImagen(rutaImagen);
+                categoriaRepository.save(categoria);
+            } catch (IOException e) {
+            }
+        }
+    }
+
+    @Transactional
+    public void delete(Integer idCategoria) {
+        // Verifica si la categoria existe antes de intentar eliminarlo
+        if (!categoriaRepository.existsById(idCategoria)) {
+            // Lanza una excepción para indicar que el usuario no fue encontrado
+            throw new IllegalArgumentException("La categoria con ID " + idCategoria + " no existe.");
+        }
+        try {
+            categoriaRepository.deleteById(idCategoria);
+        } catch (DataIntegrityViolationException e) {
+            // Lanza una nueva excepción para encapsular el problema de integridad de datos
+            throw new IllegalStateException("No se puede eliminar la categoria. Tiene datos asociados.", e);
+        }
+    }
+    // ===========================
 }
