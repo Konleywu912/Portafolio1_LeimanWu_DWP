a7970c0235face8b8398f819e4c8e2a2105fcfa1
Konleywu912 <leymanwu912@gmail.com>
2026-04-12 23:54:31 -0600

    semana#12

---
+++ src/main/java/Tienda_LeimanW/repository/VentaRepository.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Tienda_LeimanW.repository;
+
+import Tienda_LeimanW.domain.Venta;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VentaRepository extends JpaRepository<Venta, Integer>{
+    
+}
